852d770f31a2015f586e868c4033cdde1c009f3f
mike <mikebaum1@gmail.com>
2020-06-17 16:54:28 -0600

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div className="container">
+        <h4 className="center">About</h4>
+        <p>This can be Videos from motion detect cameras</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
