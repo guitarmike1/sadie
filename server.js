b496b87673184e47264d55d390f2bdd93666ffe3
mike <mikebaum1@gmail.com>
2020-07-01 15:16:05 -0600

    Added code that reads folder file names

--- server.js
+++ server.js
@@ -5,6 +5,27 @@ const express = require('express');
 
 const app = express();
 
+//requiring path and fs modules *******************************
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'src/serverSide/videos');
+console.log("directory path = ",directoryPath)
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    // handling error
+    
+    
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
+// ******************************************************
 
 app.get('/api/videos',(req, res) => {
     const videos = [
@@ -17,5 +38,5 @@ app.get('/api/videos',(req, res) => {
 });
 
 const port = 5000;
-
+console.log("mike1")
 app.listen(port, () => console.log(`started server on port  ${port}`));
