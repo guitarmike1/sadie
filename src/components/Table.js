809aba6be6d2e51994d45d228f8832a464410dfa
mike <mikebaum1@gmail.com>
2020-07-30 11:54:40 -0600

    Added correct column names. clean up

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Column, Table } from "react-virtualized";
+import { Column, Table, AutoSizer } from "react-virtualized";
 import { connect } from 'react-redux'
 // import  { videoTableAction } from "../actions/videoTableAction";
 import { videoTableAction } from "../actions/videoTableAction"
 import  { videoSelectAction } from "../actions/videoSelectAction"
+import "../css/index.css";
+import _ from "lodash";
+import "../css/styles.css";
+import "../css/App.css";
 
-// import Draggable from "react-draggable";
+import Draggable from "react-draggable";
 
 const TOTAL_WIDTH = 500;
 
@@ -30,28 +34,24 @@ class VideoTable extends React.Component {
 logArgs(event,index){
   console.log( 'here is the event:',event,
                'the index:',index,
-              //  'the object:',this.state.bpTableData[index]
   );
-  // const result = (event.rowData.fileName);
 
   this.props.videoSelectActionProperty(event.rowData.fileName)
-this.forceUpdate()
+  this.forceUpdate()
 }
 
   render() {
     console.log('Table.js props = ',this.props)
-    // console.log('TAble list = ',list)
-    // const { list } = list;
     const { list } = this.props;
     console.log("Table list = ",list.length)
     const { widths } = this.state;
     console.log('Table.js widths = ',widths)
-    // this.props.videoSelectActionProperty(1)
 
     
 
     return (
       <Table
+      
         width={TOTAL_WIDTH}
         height={300}
         headerHeight={20}
@@ -62,88 +62,79 @@ this.forceUpdate()
       >
       
         <Column
-          headerRenderer={this.headerRenderer}
+          // headerRenderer={this.headerRenderer}
           dataKey="fileName"
           label="fileName"
-          width={widths.name * TOTAL_WIDTH}
+          
+          // width={widths.name * TOTAL_WIDTH}
         />
-        {/* <Column
-          headerRenderer={this.headerRenderer}
-          dataKey="location"
-          label="Location"
-          width={widths.location * TOTAL_WIDTH}
-        /> */}
-        {/* <Column
+        <Column
+          // headerRenderer={this.headerRenderer}
+          dataKey="fileDate"
+          label="fileDate"
+          width = {300}
+          // width={widths.location * TOTAL_WIDTH}
+        />
+        <Column
           dataKey="description"
           label="Description"
           width={widths.description * TOTAL_WIDTH}
-        /> */}
+        />
         
       </Table>
     );
-    
   }
 
+  // headerRenderer = ({
+  //   columnData,
+  //   dataKey,
+  //   disableSort,
+  //   label,
+  //   sortBy,
+  //   sortDirection
+  // }) => {
+  //   return (
+  //     <React.Fragment key={dataKey}>
+  //       <div className="ReactVirtualized__Table__headerTruncatedText">
+  //         {label}
+  //       </div>
+  //       <Draggable
+  //         axis="x"
+  //         defaultClassName="DragHandle"
+  //         defaultClassNameDragging="DragHandleActive"
+  //         onDrag={(event, { deltaX }) =>
+  //           this.resizeRow({
+  //             dataKey,
+  //             deltaX
+  //           })
+  //         }
+  //         position={{ x: 0 }}
+  //         zIndex={999}
+  //       >
+  //         <span className="DragHandleIcon">⋮</span>
+  //       </Draggable>
+  //     </React.Fragment>
+  //   );
+  // };
 
-  
+  // resizeRow = ({ dataKey, deltaX }) =>
+  //   this.setState(prevState => {
+  //     const prevWidths = prevState.widths;
+  //     const percentDelta = deltaX / TOTAL_WIDTH;
 
-  
+  //     // This is me being lazy :)
+  //     const nextDataKey = dataKey === "name" ? "location" : "description";
 
-
-  headerRenderer = ({
-    columnData,
-    dataKey,
-    disableSort,
-    label,
-    sortBy,
-    sortDirection
-  }) => {
-    return (
-      <React.Fragment key={dataKey}>
-        <div className="ReactVirtualized__Table__headerTruncatedText">
-          {label}
-        </div>
-        {/* <Draggable
-          axis="x"
-          defaultClassName="DragHandle"
-          defaultClassNameDragging="DragHandleActive"
-          onDrag={(event, { deltaX }) =>
-            this.resizeRow({
-              dataKey,
-              deltaX
-            })
-          }
-          position={{ x: 0 }}
-          zIndex={999}
-        >
-          <span className="DragHandleIcon">⋮</span>
-        </Draggable> */}
-      </React.Fragment>
-    );
-  };
-
-  resizeRow = ({ dataKey, deltaX }) =>
-    this.setState(prevState => {
-      const prevWidths = prevState.widths;
-      const percentDelta = deltaX / TOTAL_WIDTH;
-
-      // This is me being lazy :)
-      const nextDataKey = dataKey === "name" ? "location" : "description";
-
-      return {
-        widths: {
-          ...prevWidths,
-          [dataKey]: prevWidths[dataKey] + percentDelta,
-          [nextDataKey]: prevWidths[nextDataKey] - percentDelta
-        }
-      };
-    });
+  //     return {
+  //       widths: {
+  //         ...prevWidths,
+  //         [dataKey]: prevWidths[dataKey] + percentDelta,
+  //         [nextDataKey]: prevWidths[nextDataKey] - percentDelta
+  //       }
+  //     };
+  //   });
 }
 
-// const test = (result) =>  {
-//   videoSelectAction(result)
-//   console.log("test *********************",result)
-// }
 
 const mapStateToProps = (state) => {
   console.log("Table.js mapStP")
