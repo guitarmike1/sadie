16caf0c7aa9ee09ba0109ec9a33853e1c397d48f
mike <mikebaum1@gmail.com>
2020-08-01 12:26:18 -0600

    adjusted look and added photos

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,31 @@
 import React from 'react'
 // import sadieUpClose from '../photos/sadieUpClose.JPG'
 import sadieWithPenguin from '../photos/sadieWithPenguin.JPG'
+import sadieInBooties from '../photos/sadieInBooties.JPG'
+import sadieRelaxing from '../photos/sadieRelaxing.JPG'
 
 const About = () => {
   return (
     <div>
       <div className="container">
         <h1 className="center  red darken-3 white-text">About</h1>
-        <h2 className="red darken-3 white-text" >Hi, my name is Sadie and I spend lots of my time hanging out looking for critters. We have lots of 
-          critters around my house and it's lots of fun to figure out where they are and then immediatley alert 
-          the neighbors. The people I live with also love it when I find a critters lurking in the yard   </h2>
-        {/* <img src={sadieUpClose} width="350" height="420"/> */}
-        <img src={sadieWithPenguin} width="350" height="420" alt=""/>
+
+        <div class="row">
+        <h1 className="col s4"></h1>
+
+          <h1 className=" col s4 orange  darken-3 white-text" >Hi, my name is Sadie and I spend lots of my time hanging out looking for critters. We have lots of 
+              critters around my house and it's lots of fun to figure out where they are and then immediatley alert 
+              the neighbors. The people I live with also love it when I find a critter lurking in the yard</h1>
+          <h2 className="col s10"></h2>
+
+          {/* <h2 className="red darken-3 white-text" >Hi, my name is Sadie and I spend lots of my time hanging out looking for critters. We have lots of 
+            critters around my house and it's lots of fun to figure out where they are and then immediatley alert 
+            the neighbors. The people I live with also love it when I find a critters lurking in the yard   </h2> */}
+          {/* <img src={sadieUpClose} width="350" height="420"/> */}
+          <img src={sadieWithPenguin} width="350" height="420" alt=""/>
+          <img src={sadieInBooties} width="420" height="350" alt=""/>
+          <img src={sadieRelaxing} width="420" height="350" alt=""/>
+        </div>
       </div>
     </div>
   )
--- src/components/Critters.js
+++ src/components/Critters.js
@@ -3,12 +3,17 @@ import bobcat1 from '../photos/bobcat1.JPG'
 
 const Critters = () => {
   return (
-    <div>
-      <div className="container">
-        <h1 className="center  orange darken-3 white-text" >Critters</h1>
-        <h2 className=" center orange  darken-3 white-text" >These are the critters I see from my back deck look-out</h2>
-        <img src={bobcat1} width="688" height="457" alt=""/>
+    <div className="container">
+    
+    <h1 className="center  orange darken-3 white-text" >Critters</h1>
+      <div class="row">
+      <h2 className=" col s4 orange  darken-3 white-text" >These are the critters I see from my back deck look-out</h2>
+      <h2 className="col s10"></h2>
+    <div class="row">
 
+        <img className src={bobcat1} width="688" height="457" alt=""/>
+
+      </div>
       </div>
     </div>
   )
